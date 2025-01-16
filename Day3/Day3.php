997380eb8fd63565c891e22cc8e4be9e937d42f3
RajuChowdhury15 <rajuchowdhury5315@gmail.com>
2025-01-16 21:33:34 +0530

    All Fill Added

---
+++ Day3/Day3.php
@@ -0,0 +1,14 @@
+<?php
+    echo"Successfully Done <br>";
+    $fname = $_POST["fname"];
+    $lname = $_POST["lname"];
+    $pass = $_POST["pass"];
+
+    echo"
+    Your FastName is $fname<br>
+    Your Last Name is $lname <br>
+    Your Password is $pass 
+    ";
+
+
+?>
